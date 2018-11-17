154cbe23e8728637e9d1b0a9a71e7d4455e7e83d
Frederik Skytte <fsk@computas.com>
2018-11-17 14:25:48 +0100

    Initial Commit

---
+++ PitchPerfect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by Frederik Skytte on 17/11/2018.
+//  Copyright © 2018 Frederik Skytte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
